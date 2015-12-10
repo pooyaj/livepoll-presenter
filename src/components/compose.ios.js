9b2516183a7740232378d9e79765180811b9b608
Simon Tse <simon.tse@hootsuite.com>
2015-12-10 12:04:26 -0800

    Adding Submit button and Android support

--- src/components/compose.ios.js
+++ src/components/compose.ios.js
@@ -3,7 +3,7 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux/native';
 
 const {
- StyleSheet, Navigator, View, Text, TextInput
+ StyleSheet, Navigator, View, Text, TextInput, TouchableHighlight
 } = React;
 
 class Compose extends React.Component {
@@ -13,9 +13,13 @@ class Compose extends React.Component {
       <View style={styles.container}>
           <TextInput
             style={styles.questionText}
-            multiline="true"
+            multiline
+            numberOfLine="3"
             placeholder="Ask your question !"
           />
+          <TouchableHighlight style={styles.submitButton}>
+              <Text>Submit</Text>
+          </TouchableHighlight>
       </View>
     );
   }
@@ -26,7 +30,7 @@ var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'flex-start',
-    alignItems: 'center',
+    alignItems: 'stretch',
     backgroundColor: '#F5FCFF',
     paddingTop: 50,
     paddingHorizontal: 20,
@@ -37,6 +41,17 @@ var styles = StyleSheet.create({
     borderColor: 'gray',
     borderWidth: 1,
     padding: 5
+  },
+  submitButton: {
+    height: 48,
+    borderColor: '#696969',
+    borderRadius: 8,
+    borderWidth: 1,
+    padding: 10,
+    margin: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#d3d3d3',
   }
 });
 
